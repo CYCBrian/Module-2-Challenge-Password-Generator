ca3ec40c0b52d15b4ac957722bd181f9b98e36c8
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2023-10-17 22:27:20 -0400

    Issue 2 - ADDED criteria variables and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -7,14 +7,36 @@ const specialCharacter = ["`", "-", "=", "[", "]", ";", "'", "\\", ",", ".", "/"
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
+  // Prompts when the generate password button is clicked
+  var passwordLength = prompt ("How long would you like the password to be? (Min. 8  Max. 128");
+  //if the number entered is less then 8 or more then 128
+  //then display alert with message "please enter a number between 8 to 128"
+  //return to previous prompt
+  //else proceed to next var fetching
+
+  var passwordLower = confirm ("Would you like the password to include lower case letters?");
+  var passwordUper = confirm ("Would you like the password to include upper case letters?");
+  var passwordNumber = confirm ("Would you like the password to include numbers?");
+  var passwordSpecial = confirm ("Would you like the password to include special characters?")
+
+  //if user did not select any of the prompts
+  //then display alert with message "please pick at least one criteria"
+  // loop back to first criteria confirm window
+  // else proceed to password generation
+
+
   var password = generatePassword();
+  
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
