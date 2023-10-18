1370019c1869aca8a1b69a9fe8881582cad75f09
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2023-10-18 18:23:58 -0400

    issue 3 - ADDED MORE PSEUDO CODEgit add -A!

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,14 @@
 //Variable for password length
-var passwordLength
+var passwordLength = []
 
 //Variables for password criteria
-var passwordLower 
-var passwordUpper 
-var passwordNumber 
-var passwordSpecial
+var criteriaLower = []
+var criteriaUpper = []
+var criteriaNumber = []
+var criteriaSpecial = []
 
 //variable for final array based on choices of criteria
-var criteriaChoice
+var criteriaChoice = []
 
 // Password Variables
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -29,45 +29,46 @@ function writePassword() {
   //if
   //no input before clicking okay  --  (!enter)
   //then
-  //display alert with message  -- alert ("please enter a number")
+  //display alert with message  -- passwordLength = alert ("please enter a number")
   //else if
   //the number entered is less then 8 or more then 128  --  (passwordLength = enter<8 || enter>128)
   //then
   //display alert with message  -- passwordLength = parseInt(prompt ("please enter a number between 8 to 128"))
   //else
-  //proceed to next var fetching
+  //proceed to criteria fetching
 
-  passwordLower = confirm ("Would you like the password to include lower case letters? Press Okay to include or Cancel to exclude.");
-  passwordUpper = confirm ("Would you like the password to include upper case letters? Press Okay to include or Cancel to exclude.");
-  passwordNumber = confirm ("Would you like the password to include numbers? Press Okay to include or Cancel to exclude.");
-  passwordSpecial = confirm ("Would you like the password to include special characters? Press Okay to include or Cancel to exclude.")
+  criteriaLower = confirm ("Would you like the password to include lower case letters? Press Okay to include or Cancel to exclude.");
+  criteriaUpper = confirm ("Would you like the password to include upper case letters? Press Okay to include or Cancel to exclude.");
+  criteriaNumber = confirm ("Would you like the password to include numbers? Press Okay to include or Cancel to exclude.");
+  criteriaSpecial = confirm ("Would you like the password to include special characters? Press Okay to include or Cancel to exclude.")
 
   //if
-  //user did not select any of the prompts  --  (!passwordLower && !passwordUpper && !passwordNumber && !passwordSpecial)
+  //user did not select any of the prompts  --  (!criteriaLower && !criteriaUpper && !criteriaNumber && !criteriaSpecial)
   //then
   //display alert with message alert  criteriaChoice = alert("please pick at least one criteria")
+  //return writePassword ()
   //else
   //if
-  //all 4 criteria chosen  --  passwordLower && passwordUpper && passwordNumber && passwordSpecial)
-  //what do i do here??? criteriaChoice = passwordLower.concat(passwordUpper, passwordNumber, passwordSpecial)? merge selected array into one giant array?
+  //all 4 criteria chosen  --  criteriaLower && criteriaUpper && criteriaNumber && criteriaSpecial)
+  //merge selected arrays into giant array --  criteriaChoice = criteriaLower.concat(criteriaUpper, criteriaNumber, criteriaSpecial)
   //else
   //if
   //3 criteria chosen (list of else if statements - permutation of 3 of the 4 criteria)
+  //merge selected arrays into giant array --  criteriaChoice = criteriaLower.concat(criteriaUpper, criteriaSpecial)
   //else
   //if
   //2 criteria chosen (list of else if statements - permutation of 2 of the 4 criteria)
+  //merge selected arrays into giant array --  criteriaChoice = criteriaLower.concat(criteriaUpper)
   //else
   //if
   //1 criteria chosen (list of else if statement - each of the criteria)
   //proceed to password generation
 
+  // 
   var password = generatePassword();
-
-  //generatePassword function?
-  //this is the part that sucks right?
-  
-
-
+    // for loop?  --  for (let i=0; i < passwordLength; i++)
+    // this is the part that sucks
+    // mathRandom
 
   var passwordText = document.querySelector("#password");
 
